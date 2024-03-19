cfd7c38e275c204dbba6c9e41b61a8b5486a5d83
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2024-03-19 10:07:24 +0500

    project setup

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, RouterModule],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor() { }
+
+  ionViewWillEnter() {
+    console.log('enter')
+  }
+
+  ionViewDidLeave() {
+    console.log('Leave')
+  }
 }
